ca77d8bdf0d8baf5b25b1fa5f4423ba8886a0869
RobertCorreaT <ranireto@gmail.com>
2025-05-02 17:34:46 -0400

    feat: componente para agregar nuevos proyectos al portafolio
    - Añadido formulario con funcionalidad controlada para capturar el título, categoría, descripción corta y estado del proyecto.
    - Integrado un espacio para la funcionalidad de Editor.js en el contenido del proyecto.
    - Configurados botones de vista previa y guardado, con opciones para "Borrador" y "Publicar".
    - Redirige o muestra un mensaje de éxito después del envío del formulario (preparado para integración futura).

--- components/editorJs.tsx
+++ components/editorJs.tsx
@@ -216,20 +216,20 @@ export default function EditorJs() {
         }
     }, [])
 
-    const handleSave = () => {
-        ref.current?.save().then(async (savedData)=>{
-            console.log(savedData, 'desde handleSave')
-            await fetch('/api/blog', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    content: savedData
-                })
-            })
-        })
-    }
+    // const handleSave = () => {
+    //     ref.current?.save().then(async (savedData)=>{
+    //         console.log(savedData, 'desde handleSave')
+    //         await fetch('/api/blog', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify({
+    //                 content: savedData
+    //             })
+    //         })
+    //     })
+    // }
 
     return (
         <>
